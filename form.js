70d73bd5513dc9f6a80d9786f9036268a508cf9c
Tamilarasi-k <49061914+Tamilarasi-k@users.noreply.github.com>
2019-04-22 17:22:13 +0530

    Update form.js

--- form.js
+++ form.js
@@ -22,14 +22,24 @@ class form extends Component{
   }
 }
 render(){
-    const { name, age } = this.state;
+    const {RollNo,Name,Tamil,English,Maths,Science,Social} = this.state;
     return(
 <form>
 
+<label>Rollno</label>
+    <input type ="text" name ="RollNo" value ={RollNo} onChange={this.handlechange}/><br/><br/>
     <label>Name</label>
-    <input type ="text" name ="name" value ={name} onChange={this.handlechange}/>
-    <label>Age</label>
-    <input type ="text" name ="age" value ={age} onChange ={this.handlechange}/>
+    <input type ="text" name ="Name" value ={Name} onChange ={this.handlechange}/><br/><br/>
+    <label>Tamil</label>
+    <input type ="text" name ="Tamil" value ={Tamil} onChange ={this.handlechange}/><br/><br/>
+    <label>English</label>
+    <input type ="text" name ="English" value ={English} onChange ={this.handlechange}/><br/><br/>
+    <label>Maths</label>
+    <input type ="text" name ="Maths" value ={Maths} onChange ={this.handlechange}/><br/><br/>
+    <label>Science</label>
+    <input type ="text" name ="Science" value ={Science} onChange ={this.handlechange}/><br/><br/>
+    <label>Social</label>
+    <input type ="text" name ="Social" value ={Social} onChange ={this.handlechange}/><br/><br/>
     <input type ="button" value="Submit" onClick ={this.submitForm} />
 </form>
     );
